565080caa9dbedeeebaed7df21e33c7f55473286
Sammwy <sammwy.dev@gmail.com>
2022-12-22 13:58:36 -0300

    feature: 💎 added bits tests

---
+++ tests/bits.test.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { TwitchAPI } from '../src';
+
+const twitch = new TwitchAPI({
+  clientId: process.env['TWITCH_CLIENT_ID'] as string,
+  accessToken: process.env['TWITCH_ACCESS_TOKEN'] as string,
+});
+
+test('Get bits leaderboard', async () => {
+  const leaderboard = await twitch.bits.getLeaderboard({ count: 5 });
+  expect(leaderboard.length).toBeGreaterThanOrEqual(5);
+});
